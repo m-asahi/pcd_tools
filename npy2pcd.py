548f9d2b66608d259c5da3908c86b7ee42a83322
ASAHI,Michiharu <m-asahi@aist.go.jp>
2020-03-25 15:39:40 +0900

    add show-colormap

--- npy2pcd.py
+++ npy2pcd.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import random
 import numpy as np
 import open3d as o3d
 import matplotlib.pyplot as plt
@@ -17,10 +18,42 @@ def color_map(n, map):
 	npcolmap = np.insert(npcolmap, 0, z, axis=0)
 	return npcolmap
 
-parser = OptionParser("usage: %prog [options] npy_file\nConvert numpy XYZRGB format to PCD format.")
+
+def generate_pcd(output, colmap):
+	n = len(colmap)
+	points = []
+	colors = []
+	size = 10000
+	for x in np.arange(0, n, 1/size):
+		r = random.random() + int(x)
+		y = random.random()
+		points.append([r, y, 0])
+		colors.append(npcolmap[int(x)])
+	pcd = o3d.geometry.PointCloud()
+	pcd.points = o3d.utility.Vector3dVector(np.array(points))
+	pcd.colors = o3d.utility.Vector3dVector(np.array(colors))
+	o3d.io.write_point_cloud(output, pcd)
+
+parser = OptionParser("usage: %prog [options] npy_txt_file\nConvert numpy XYZ-Label format to PCD format with Colormap.")
+parser.add_option('-c', '--classes', help='number of classes (required)', type=int, metavar='CLASSES')
 parser.add_option('-o', '--output', help='output filename', metavar='FILE')
+parser.add_option('-s', '--show-colormap', help='show colormap', action='store_true')
+parser.add_option('--colormap', help='pyplot colormap name', default='Paired', metavar='COLORMAP')
 
 (options, args) = parser.parse_args()
+if options.classes is None:
+	print('error! --classes is required.')
+	sys.exit(1)
+
+if options.show_colormap:
+	n = options.classes
+	map = options.colormap
+	npcolmap = color_map(n, map)
+	# print(npcolmap)
+	output = options.output or '{0}-{1}.pcd'.format(map, n)
+	generate_pcd(output, npcolmap)
+	sys.exit(0)
+
 if len(args) == 0:
 	parser.print_usage()
 	sys.exit(1)
